81b103863b14af510f236e37f783021183c722f6
ito-72 <153911389+ito-72@users.noreply.github.com>
2025-08-11 15:25:31 +0900

    Add files via upload

--- api/config.js
+++ api/config.js
@@ -1,4 +1,4 @@
 // GASの公開URLをここに設定
 const CONFIG = {
-  GAS_URL: 'https://script.google.com/macros/s/AKfycbx4qcstwe7PhTdYPi3EGZJ8DZMp5lGtnTbYCkm_Hr6iNkQbZCLvtQEAjtbyjAwcdAPj/exec'
+  GAS_URL: 'https://script.google.com/macros/s/AKfycby3v1oLf4Iatw3NCSoqllocecd37zU5ZWVgLC6Wjsp4Ymn-A3ki_ucJHjQa6IW85jei/exec'
 };
